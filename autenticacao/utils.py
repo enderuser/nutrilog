5ea17591a254a619cf0a4cc243e57ebf7866fa97
Wender Monteiro <enderuser@outlook.com>
2022-12-08 06:21:32 -0300

    andamento da aula

---
+++ autenticacao/utils.py
@@ -0,0 +1,18 @@
+import re
+from django.contrib import messages
+from django.contrib.messages import constants
+
+def password_is_valid(request, password, confirm_password):
+    if len(password) < 6:
+        messages.add_message(request, constants.ERROR, 'Senha menor que 6 caracteres não permitido')
+        return False
+    
+    if not password == confirm_password:
+        messages.add_message(request, constants.ERROR, 'As senhas informadas devem ser iguais')
+        return False
+    
+    if re.search('[a-z]', password):
+        messages.add_message(request, constants.ERROR, 'Senha não possui letras de a-z')
+        return False
+    
+    return True
--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,8 +1,25 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .utils import password_is_valid
+from django.contrib.auth.models import User
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        usuario = request.POST.get('usuario')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        if not password_is_valid(request, senha, confirmar_senha):
+            return redirect('cadastro')
+        
+        try:
+            user = User.objects.create_user(username = usuario, password = senha, is_active = False)
+            user.save()
+            return redirect('logar')
+        except:
+            return redirect('cadastro')
 
 def logar(request):
     return HttpResponse('Vc está na pagina de login')
